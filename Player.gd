df71640a7ca3f30d0e36524e7ac024ddd80df381
Franziskus <79016562+franziskusz@users.noreply.github.com>
2024-01-31 17:04:59 +0100

    WIP update Player

--- Player.gd
+++ Player.gd
@@ -1,17 +1,68 @@
 extends Area2D
 
+@export var speed: int = 400 # How fast the player will move (pixels/sec).
+@export var player_position: Vector2
+var input_position: Vector2
+var is_bot: bool = false
+var bot_direction: Vector2 = Vector2(-0.5,0.2)
+var screen_size: Vector2 # Size of the game window.
+
+
 signal hit
 
-@export var speed = 400 # How fast the player will move (pixels/sec).
-var screen_size # Size of the game window.
+signal send_player_position(player_position: Vector2)
+
+func _on_Player_body_entered(_body):
+	#hide() # Player disappears after being hit.
+	hit.emit()
+	# Must be deferred as we can't change physics properties on a physics callback.
+	#$CollisionShape2D.set_deferred(&"disabled", true)
+	
+func start(pos):
+	position = pos
+	player_position = pos
+	input_position = pos
+	show()
+	$CollisionShape2D.disabled = false
+	
+	if is_bot:
+		$BotTimer.set_wait_time(2.0)
+		$BotTimer.time_out.connect(update_bot_direction())
+		$BotTimer.start()
+
+
+func despawn():
+	hide()
+	
+func update_bot_mode(bot_mode: bool):
+	is_bot = bot_mode
+
+func update_bot_direction():
+	if bot_direction == Vector2.UP:
+		bot_direction = Vector2.RIGHT
+	elif bot_direction == Vector2.RIGHT:
+		bot_direction = Vector2.DOWN
+	elif bot_direction == Vector2.DOWN:
+		bot_direction = Vector2.LEFT
+	elif bot_direction == Vector2.LEFT:
+		bot_direction = Vector2.UP
+	else:
+		bot_direction = Vector2.UP
 
 func _ready():
 	screen_size = get_viewport_rect().size
 	hide()
+	
+	$Main/HUD.connect("bot_player_switch", update_bot_mode(false))
+	$Main/HUD.connect("stop_game", despawn())
 
 
 func _process(delta):
 	var velocity = Vector2.ZERO # The player's movement vector.
+	
+	if is_bot:
+		velocity += bot_direction
+	
 	if Input.is_action_pressed(&"move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed(&"move_left"):
@@ -23,31 +74,35 @@ func _process(delta):
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		$AnimatedSprite2D.play()
+		
+		if velocity.x != 0:
+			$AnimatedSprite2D.animation = &"right"
+			$AnimatedSprite2D.flip_v = false
+			$Trail.rotation = 0
+			$AnimatedSprite2D.flip_h = velocity.x < 0
+		elif velocity.y != 0:
+			$AnimatedSprite2D.animation = &"up"
+			$AnimatedSprite2D.flip_v = velocity.y > 0
+			$Trail.rotation = PI if velocity.y > 0 else 0	
+		
+		var change = velocity * delta
+		var position = get_global_position() + change
+		var position_clamped = position.clamp(Vector2.ZERO, screen_size)
+		
+		input_position = position_clamped
+		
+		$AnimatedSprite2D.play()	
 	else:
 		$AnimatedSprite2D.stop()
 
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, screen_size)
 
-	if velocity.x != 0:
-		$AnimatedSprite2D.animation = &"right"
-		$AnimatedSprite2D.flip_v = false
-		$Trail.rotation = 0
-		$AnimatedSprite2D.flip_h = velocity.x < 0
-	elif velocity.y != 0:
-		$AnimatedSprite2D.animation = &"up"
-		$AnimatedSprite2D.flip_v = velocity.y > 0
-		$Trail.rotation = PI if velocity.y > 0 else 0
-
-func start(pos):
-	position = pos
-	show()
-	$CollisionShape2D.disabled = false
+func _physics_process(delta):
+	var latest_input_position = input_position
+	
+	if player_position != latest_input_position:
+		player_position = latest_input_position
+		set_global_position(latest_input_position)
+		
+		emit_signal("send_player_position", latest_input_position)
 
 
-func _on_Player_body_entered(_body):
-	hide() # Player disappears after being hit.
-	hit.emit()
-	# Must be deferred as we can't change physics properties on a physics callback.
-	$CollisionShape2D.set_deferred(&"disabled", true)
