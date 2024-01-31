d2215fbfe552b55b0469d199d54046d4c32f9bd9
Franziskus <79016562+franziskusz@users.noreply.github.com>
2024-01-31 17:41:23 +0100

    WIP update Mob.gd

--- Mob.gd
+++ Mob.gd
@@ -1,10 +1,75 @@
 extends RigidBody2D
 
+const INITIAL_FORCE_DIVISOR: float = 35.0
+const AIMING_FORCE_DIVISOR: float = 5.0
+
+var min_speed: float = 150.0
+var max_speed: float = 250.0
+var direction: float = 0.0
+var speed: float = 0.0
+var velocity: Vector2 = Vector2(0.0, 0.0)
+var target: Vector2 = Vector2(0.0, 0.0)
+var aiming_direction: Vector2 = Vector2(0.0, 0.0)
+var initial_direction: Vector2 = Vector2(0.0, 0.0)
+
+signal despawned() #deprecated
+
+func _on_VisibilityNotifier2D_screen_exited():
+	aim_at_player()
+	#queue_free()
+	
+func on_start_game(): #deprecated
+	queue_free()
+	
+func update_target(player_position: Vector2):
+	target = player_position
+	update_aiming_direction()
+
+func update_aiming_direction():
+	var direction_var = get_position().angle_to_point(target)
+	aiming_direction = Vector2(speed, 0.0).rotated(direction)
+	
+func aim_at_player():
+	var target_var = target
+	look_at(target_var)
+	var direction_var = get_position().angle_to_point(target)
+	
+	velocity = Vector2(speed, 0.0).rotated(direction_var)
+	var velocity_var = velocity
+	set_linear_velocity(velocity_var)
+	
+	aiming_direction = velocity_var
+	initial_direction = velocity_var
+
+func on_game_over_despawn():
+	queue_free()
+
 func _ready():
 	$AnimatedSprite2D.play()
 	var mob_types = Array($AnimatedSprite2D.sprite_frames.get_animation_names())
 	$AnimatedSprite2D.animation = mob_types.pick_random()
+	
+	$Player.send_player_position.connect(update_target(Vector2(0.0,0.0)))
+	$HUD.stop_game.connect(on_game_over_despawn())
+	
+	$HUD.start_game.connect(on_start_game())
+	
+	var target_var = $Player.get_position()
+	target = target_var
+	
+	speed = randf_range(min_speed, max_speed)
+	
+	aim_at_player()
 
+func _physics_process(delta):
+	var initial_force_divisor = Vector2(INITIAL_FORCE_DIVISOR, INITIAL_FORCE_DIVISOR)
+	var aiming_force_divisor = Vector2(AIMING_FORCE_DIVISOR, AIMING_FORCE_DIVISOR)
+	var aiming_direction_var = aiming_direction / aiming_force_divisor
+	apply_force(aiming_direction_var)
+	var initial_direction_var = initial_direction / initial_force_divisor
+	apply_force(initial_direction_var)
+	
+func _integrate_forces(state):
+	var target_var = target
+	look_at(target)
 
-func _on_VisibilityNotifier2D_screen_exited():
-	queue_free()
